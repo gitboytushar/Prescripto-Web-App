72abbedb244ca5183349b23c2423828ac9161034
Tushar Verma <123294028+gitboytushar@users.noreply.github.com>
2025-02-01 16:29:35 +0530

    Cloudinary Setup: web account and prescripto api setup, code setup

--- backend/server.js
+++ backend/server.js
@@ -2,11 +2,13 @@ import express from 'express'
 import cors from 'cors'
 import 'dotenv/config'
 import connetDB from './config/mongodb.js'
+import connectCloudinary from './config/cloudinary.js'
 
 // app config
 const app = express()
 const port = process.env.PORT || 4000
 connetDB()
+connectCloudinary()
 
 // middlewares
 app.use(express.json())
