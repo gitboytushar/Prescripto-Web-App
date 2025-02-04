d428d9f71e41375ad56ce2f7f39f446633ee467a
Tushar Verma <123294028+gitboytushar@users.noreply.github.com>
2025-02-04 09:38:37 +0530

    API created for add-doctor and admin-auth and other important codes for route and multer
    Issue: The image file is not getting uploaded on cloudinary on api testing via thunder client

---
+++ backend/controllers/adminController.js
@@ -0,0 +1,113 @@
+import validator from 'validator'
+import bcrypt from 'bcrypt'
+import { v2 as cloudinary } from 'cloudinary'
+import doctorModel from '../models/doctorModel.js'
+import jwt from 'jsonwebtoken'
+
+// Api for adding Doctor
+const addDoctor = async (req, res) => {
+  try {
+    const {
+      name,
+      email,
+      password,
+      speciality,
+      degree,
+      experience,
+      about,
+      fees,
+      address
+    } = req.body
+    const imageFile = req.file
+
+    // checking for all data to add doctor
+    if (
+      !name ||
+      !email ||
+      !password ||
+      !speciality ||
+      !degree ||
+      !experience ||
+      !about ||
+      !fees ||
+      !address
+    ) {
+      return res.json({ success: false, message: 'Missing Details.' })
+    }
+
+    // validating emailing format
+    if (!validator.isEmail(email)) {
+      return res.json({
+        success: false,
+        message: 'Please Enter a Valid Email.'
+      })
+    }
+
+    // validating strong password
+    if (password.length < 8) {
+      return res.json({
+        success: false,
+        message: 'Please Enter a Strong Password. (min. 8 characters)'
+      })
+    }
+
+    // hashing doctor password
+    const salt = await bcrypt.genSalt(10)
+    const hashedPassword = await bcrypt.hash(password, salt)
+
+    // upload image to cloudinary
+    const imageUpload = await cloudinary.uploader.upload(imageFile.path, {
+      resource_type: 'image'
+    })
+    const imageUrl = imageUpload.secure_url
+
+    // doctor data
+    const doctorData = {
+      name,
+      email,
+      image: imageUrl,
+      password: hashedPassword,
+      speciality,
+      degree,
+      experience,
+      about,
+      fees,
+      address: JSON.parse(address),
+      date: Date.now()
+    }
+
+    const newDoctor = new doctorModel(doctorData)
+    await newDoctor.save()
+
+    res.status(201).json({ success: true, message: 'Doctor Added 🎉' })
+  } catch (error) {
+    console.log(error)
+    res
+      .status(500)
+      .json({ success: false, message: `Server error: ${error.message}` })
+  }
+}
+
+// API for the admin Login
+const loginAdmin = async (req, res) => {
+  try {
+    const { email, password } = req.body
+
+    if (
+      email === process.env.ADMIN_EMAIL &&
+      password === process.env.ADMIN_PASSWORD
+    ) {
+      const token = jwt.sign(email + password, process.env.JWT_SECRET)
+      return res.json({ success: true, message: 'Admin Logged In 🎉', token })
+    } else {
+      return res.json({ success: false, message: 'Invalid Credentials.' })
+    }
+  } catch (error) {
+    console.log(error)
+    res
+      .status(500)
+      .json({ success: false, message: `Server error: ${error.message}` })
+  }
+}
+
+export { addDoctor, loginAdmin }
--- backend/server.js
+++ backend/server.js
@@ -3,23 +3,26 @@ import cors from 'cors'
 import 'dotenv/config'
 import connetDB from './config/mongodb.js'
 import connectCloudinary from './config/cloudinary.js'
+import adminRouter from './routes/adminRoute.js'
 
-// app config
+// -------- app config ----------
 const app = express()
 const port = process.env.PORT || 4000
 connetDB()
 connectCloudinary()
 
-// middlewares
+// -------- middlewares ---------
 app.use(express.json())
 app.use(cors())
 
-// api endpoints
+// ------ api endpoints ------
+app.use('/api/admin', adminRouter) // localhost:4000/api/admin/add-doctor
+
 app.get('/', (req, res) => {
   res.send('API WORKING...')
 })
 
-// port listen
+// -------- port listen -------
 app.listen(port, () => {
   console.log('Server Running on port', port)
 })
